ceea371e9fad21a3a6a9e2e100d006ba3d663be4
Satraj <satraj@thebhouse.com>
2015-03-16 23:16:52 -0400

    Initial Commit

---
+++ SBLoader/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SBLoader
+//
+//  Created by Satraj Bambra on 2015-03-16.
+//  Copyright (c) 2015 Satraj Bambra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
